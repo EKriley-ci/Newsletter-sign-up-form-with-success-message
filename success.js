d063d73672ea2b90f5af5854c235d325b1e66a49
EKriley-ci <omahlayghost@gmail.com>
2024-11-27 22:12:04 +0000

    freez

--- success.js
+++ success.js
@@ -1,6 +1,8 @@
 // Récupérer l'email depuis localStorage
 const userMail = document.getElementById('userMail');
 const email = localStorage.getItem('userEmail');
+const dismissBtn = document.getElementById('dismiss')
+const objet = document.querySelector('.objet'); // Ajout de la récupération de l'objet animé
 
 // Vérifier si un email est présent
 if (email) {
@@ -8,3 +10,13 @@ if (email) {
 } else {
     userMail.textContent = "No email found.";
 }
+
+dismissBtn.addEventListener('click', () => {
+     // Afficher la barre de chargement et masquer le formulaire
+     loadbar.style.display = 'flex';
+
+     // Attendre la fin de l'animation avant de rediriger
+     objet.addEventListener('animationend', () => {
+         window.location.href = 'index.html'; // Rediriger vers la page de d'acceuil
+     });
+})
